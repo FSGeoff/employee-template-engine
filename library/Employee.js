6e1c852aae4fd0c183d8900552e20dc306102f78
Geoff Nix <FullStackGeoff@gmail.com>
2020-11-07 09:42:02 -0500

    got first 6 tests to pass

--- library/Employee.js
+++ library/Employee.js
@@ -1,23 +1,17 @@
-const inquirer = require("inquirer");
-
 class Employee {
-	constructor(name, email) {
+	constructor(name, id, email) {
 		this.name = name;
+		this.id = id;
 		this.email = email;
 	}
 	getName() {
 		return this.name;
 	}
-
 	getId() {
 		return this.id;
 	}
-
 	getEmail() {
 		return this.email;
-	}
-	getRole() {
-		return "Employee";
 	}
 }
 
--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,9 +1,29 @@
+const { expect } = require("@jest/globals");
 const Employee = require("../library/Employee");
 
-describe("Employee", () => {
-	describe("Initialization", () => {
-		it("Should accept a name", () => {
-			expect(new Employee("Geoff Nix").name).toEqual("Geoff Nix");
-		});
+describe("Employee class", () => {
+	it("should accept a name", () => {
+		const worker = new Employee("Geoff Nix", 1, "nicewithands@gmail.com");
+		expect(worker.name).toEqual("Geoff Nix");
+	});
+	it("should accept a number", () => {
+		const worker = new Employee("Geoff Nix", 1, "nicewithands@gmail.com");
+		expect(worker.id).toEqual(1);
+	});
+	it("should accept an email", () => {
+		const worker = new Employee("Geoff Nix", 1, "nicewithands@gmail.com");
+		expect(worker.email).toEqual("nicewithands@gmail.com");
+	});
+	it("getName function should return name", () => {
+		const worker = new Employee("Geoff Nix", 1, "nicewithands@gmail.com");
+		expect(worker.getName()).toEqual("Geoff Nix");
+	});
+	it("getId function should return Id", () => {
+		const worker = new Employee("Geoff Nix", 1, "nicewithands@gmail.com");
+		expect(worker.getId()).toEqual(1);
+	});
+	it("getEmail function should return email", () => {
+		const worker = new Employee("Geoff Nix", 1, "nicewithands@gmail.com");
+		expect(worker.getEmail()).toEqual("nicewithands@gmail.com");
 	});
 });
